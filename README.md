e3b0dd3e4f802aec6fde4d7a1dbb1cacaead3daf
ibrezm1 <ibrezm1@gmail.com>
2022-06-05 15:50:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ekgyu9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ekgyu9)
---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
+    res.end();
+  })
+  .listen(8080);
+
+// Use Command C to stop
